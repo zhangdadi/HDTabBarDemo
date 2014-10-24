0068b01a86175904cccdca3b0465712b5547a770
张达棣 <zhangdadi@zhangdadideMac-mini.local>
2014-10-24 13:40:17 +0800

    1

---
+++ ZTabbarDemo/ZTabbar/ZUIViewController.m
@@ -0,0 +1,36 @@
+//
+//  ZUIViewController.m
+//  ZTabbarDemo
+//
+//  Created by 张达棣 on 14-5-22.
+//  Copyright (c) 2014年 张达棣. All rights reserved.
+//
+
+#import "ZUIViewController.h"
+
+@implementation ZUIViewController
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ZTabbarDemo/aViewController.h
@@ -0,0 +1,13 @@
+//
+//  aViewController.h
+//  ZTabbarDemo
+//
+//  Created by 张达棣 on 14-5-21.
+//  Copyright (c) 2014年 张达棣. All rights reserved.
+//
+
+#import "ZUIViewController.h"
+
+@interface aViewController : ZUIViewController
+
+@end
